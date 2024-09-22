c95e564a15c68a5ccb0a11fb03cae57f29466954
kyanil82 <kumar1982.anil@gmail.com>
2024-09-22 16:33:57 +0200

    initial

---
+++ sb-multithreading/src/main/java/com/aky/sb_multithreading/SbMultithreadingApplication.java
@@ -0,0 +1,13 @@
+package com.aky.sb_multithreading;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbMultithreadingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SbMultithreadingApplication.class, args);
+	}
+
+}
---
+++ sb-multithreading/src/test/java/com/aky/sb_multithreading/SbMultithreadingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aky.sb_multithreading;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SbMultithreadingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
